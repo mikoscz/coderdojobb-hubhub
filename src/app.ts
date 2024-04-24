3a45ca0c5117eceb940e82f5e082e24787d080ee
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2024-04-24 17:12:01 +0200

    feat: first endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("Hello World");
+import express, { Request, Response } from "express";
+
+export function build() {
+  const app = express();
+
+  app.get("/test", (_req: Request, res: Response) => {
+    res.json({
+      test: "hello",
+    });
+  });
+
+  return app;
+}
--- src/bin/api.ts
+++ src/bin/api.ts
@@ -1,1 +1,9 @@
-console.log('api.ts');
+import { build } from "../app";
+
+const app = build();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`HubHub is listening on port ${port} http://localhost:${port}`);
+});
